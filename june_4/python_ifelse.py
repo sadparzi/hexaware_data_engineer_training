7e5048190e162f68b24feff53979b5ca78b13186
sadparzi <shaanroj@gmail.com>
2026-06-04 11:15:31 +0530

    if else in python

---
+++ june_4/python_ifelse.py
@@ -0,0 +1,15 @@
+
+salary = 35000
+
+if salary > 50000:
+    print("High Income")
+else:
+    print("Normal Income")
+
+salary1 = 75000
+experience = 5
+
+if salary1 > 50000 and experience >= 3:
+    print("Eligible")
+else:
+    print("Not Eligible")
