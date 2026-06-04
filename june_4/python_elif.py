c31c9c6a4854acfdb576dfd12e3ce007ca5d26f4
sadparzi <shaanroj@gmail.com>
2026-06-04 11:16:46 +0530

    elif statements in python

---
+++ june_4/python_elif.py
@@ -0,0 +1,12 @@
+#elif statements in python
+
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Grade D")
