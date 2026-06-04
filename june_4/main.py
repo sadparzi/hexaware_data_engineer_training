5d57a93bb50788988935d7261fca95af8f59d752
sadparzi <shaanroj@gmail.com>
2026-06-04 11:14:08 +0530

    types of data in python

---
+++ june_4/main.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+
+customer_name = "Rahul Sharma"
+age = 28
+salary = 75000.50
+is_active = True
+
+print(customer_name)
+print(age)
+print(salary)
+print(is_active)
+
+print(type(customer_name))
+print(type(age))
+print(type(salary))
+print(type(is_active))
